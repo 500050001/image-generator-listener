764d7ff13686c3c89e6baa5d8e3341fd39c451bc
70007000 <123183575+70007000@users.noreply.github.com>
2024-08-20 23:47:21 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+
 const { INFURA_PROJECT_ID, DEPLOYER_PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-    }
+    goerli: {
+      url: `https://sepolia.infura.io/v3/6e8ee0b6eb3d4b5f8c403d2780293b0f`,
+      accounts: [`0x${DEPLOYER_PRIVATE_KEY}`],
+    },
+    // other networks...
   }
 };
