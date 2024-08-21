9f29d304353f6d35be6184d38e004b5d46f8518e
70007000 <123183575+70007000@users.noreply.github.com>
2024-08-21 20:19:10 +0200

    Update deploy.js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -2,8 +2,13 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
+  console.log("Using Infura Project ID:", process.env.INFURA_PROJECT_ID);
+  console.log("Private key length:", process.env.DEPLOYER_PRIVATE_KEY.length);
+
   const balance = await deployer.getBalance();
   console.log("Account balance:", balance.toString());
+
+  // Your deployment logic here
 }
 
 main()
