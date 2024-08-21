a90d3f3d52c28ffbbb6694407ef0753ab39cfcfa
70007000 <123183575+70007000@users.noreply.github.com>
2024-08-21 20:16:42 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,12 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-  solidity: "0.8.0",  // Specify your Solidity version
+  solidity: "0.8.0",
   networks: {
-    sepolia: {  // Configure Sepolia network
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,  // Use Sepolia's Infura endpoint
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`]  // Your private key with '0x' prefix
-    }
-  }
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
+  },
 };
 
