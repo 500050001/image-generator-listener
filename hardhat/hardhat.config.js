a196befd61bcc9fef668a6ab26a602e9ea0da9e3
70007000 <123183575+70007000@users.noreply.github.com>
2024-08-21 20:28:58 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,8 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/6e8ee0b6eb3d4b5f8c403d2780293b0f`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+      accounts: [`0xbf0a2f2a792715672918b67b869eccd7df1eb5d2ebc27e5672cbf2439975175d
+`],
     },
   },
 };
