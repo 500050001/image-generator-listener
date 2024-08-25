46c9992ae853e31ca249c46a59e687daaf982554
70007000 <123183575+70007000@users.noreply.github.com>
2024-08-25 13:26:44 +0200

    Update hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY || "";
+
+if (!INFURA_PROJECT_ID || !DEPLOYER_PRIVATE_KEY) {
+  console.error("Error: INFURA_PROJECT_ID or DEPLOYER_PRIVATE_KEY is not set.");
+  process.exit(1);
+}
+
 module.exports = {
   defaultNetwork: "sepolia",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY] // No need to add `0x` again here
+      url: `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [DEPLOYER_PRIVATE_KEY]
     }
   },
   solidity: "0.8.0",
